5642c9401b98a1d9feafc25a8c9566cca84d407e
Alexia Chan <achan124@uw.edu>
2025-03-22 00:35:51 -0700

    changes

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -8,27 +8,18 @@ export function Homepage() {
             <div className="contentBody">
                 <h1>Alexia Chan</h1>
                 <h2 className="homepageRole">Frontend Developer</h2>
-                <p className="homepageAbout">I am an undergraduate Informatics student at the University of Washington, specialising in front-end development with an interest in user experience design. Based in Seattle WA, I’m passionate about turning ideas into functional, visually appealing interfaces.</p>
-                <button>Check out my work!</button>
+                <p className="homepageAbout">I'm an undergraduate Informatics student at the University of Washington, specializing in front-end development with an interest in user experience design. Based in Seattle WA, I’m passionate about turning ideas into functional, visually appealing interfaces.</p>
+                <button className="mb-5">Check out my work!</button>
 
-                {/* <div className="d-flex flex-wrap justify-content-center gap-5 m-5">
-                    <div className="projectImg">
-                        <img src=".././public/img/dawgRide.png" width={500}/>
+                <div className="d-flex flex-wrap justify-content-center gap-5 m-5 homepageProjects">
+                    <div className="imgBox">
+                        <img src=".././public/img/dawgRide.png"/>
                     </div>
-                    <div className="projectImg">
-                        <img src=".././public/img/smiskiGrove.png" width={500}/>
-                    </div>
-                    <div className="projectImg">
-                        <img src=".././public/img/mbti.png" width={500}/>
-                    </div>
-                    <div className="projectImg">
-                        <img src=".././public/img/NPS.png" width={500}/>
+                    <div className="imgBox">
+                        <img src=".././public/img/smiskiGrove.png"/>
                     </div>
 
-                </div> */}
-
-
-
+                </div>
             </div>
             <Footer />
             
