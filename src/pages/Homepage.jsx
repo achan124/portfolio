6a32d1826fece8b1f75c1872e6c3a1de5535bf75
Alexia Chan <achan124@uw.edu>
2025-03-22 20:14:32 -0700

    homepage styling

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,6 @@
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
+import { ContactForm } from "../components/ContactForm";
 
 export function Homepage() {
     return (
@@ -9,17 +10,18 @@ export function Homepage() {
                 <h1>Alexia Chan</h1>
                 <h2 className="homepageRole">Frontend Developer</h2>
                 <p className="homepageAbout">I'm an undergraduate Informatics student at the University of Washington, specializing in front-end development with an interest in user experience design. Based in Seattle WA, I’m passionate about turning ideas into functional, visually appealing interfaces.</p>
-                <button className="mb-5">Check out my work!</button>
+                <a href="#projects" className="checkout-link">Check out my work!</a>
 
-                <div className="d-flex flex-wrap justify-content-center gap-5 m-5 homepageProjects">
+                <div id="projects" className="d-flex flex-wrap justify-content-center gap-5 m-5 homepageProjects">
                     <div className="imgBox">
                         <img src=".././public/img/dawgRide.png"/>
                     </div>
                     <div className="imgBox">
                         <img src=".././public/img/smiskiGrove.png"/>
                     </div>
-
                 </div>
+
+                <ContactForm />
             </div>
             <Footer />
             
